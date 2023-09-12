6a29b918d96fb4b63bf6b08e7e7ce89fa6c4000b
xhyu <xhyu@qeelyn.com>
2023-09-12 16:36:29 +0800

    使用缓存避免多次取数

--- src/services/appConfig.ts
+++ src/services/appConfig.ts
@@ -5,31 +5,31 @@ const ICE_APP_CONFIG_ADDRESS = process.env.ICE_APP_CONFIG_ADDRESS ?? '',
   appConfig: AppConfig[] = [];
 
 export const getAppConfigs = async () => {
-  if (ICE_APP_CONFIG_ADDRESS) {
-    if (appConfig.length === 0) {
+  if (appConfig.length === 0) {
+    if (ICE_APP_CONFIG_ADDRESS) {
       const result: AppConfig[] = await request.get(ICE_APP_CONFIG_ADDRESS);
       if (Array.isArray(result)) {
         appConfig.push(...result)
       }
+    } else {
+      appConfig.push({
+        path: '/ucenter',
+        name: "app1",
+        title: '用户中心',
+        loadScriptMode: "fetch",
+        sandbox: true,
+        entry: 'http://127.0.0.1:8801',
+      },
+        // {
+        //   path: '/msg',
+        //   name: "msg",
+        //   title: '消息中心',
+        //   loadScriptMode: "fetch",
+        //   sandbox: true,
+        //   entry: 'http://127.0.0.1:3002',
+        // }
+      )
     }
-  } else {
-    appConfig.push({
-      path: '/ucenter',
-      name: "app1",
-      title: '用户中心',
-      loadScriptMode: "fetch",
-      sandbox: true,
-      entry: 'http://127.0.0.1:8801',
-    },
-      // {
-      //   path: '/msg',
-      //   name: "msg",
-      //   title: '消息中心',
-      //   loadScriptMode: "fetch",
-      //   sandbox: true,
-      //   entry: 'http://127.0.0.1:3002',
-      // }
-    )
   }
   return appConfig;
 }
